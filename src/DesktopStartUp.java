c7336e3de77ee1ef238eee86bf1133c4027c9242
fofo13 <fofosav85@hotmail.it>
2012-11-03 19:11:10 +0100

    Signed-off-by: fofo13 <fofosav85@hotmail.it>

--- src/DesktopStartUp.java
+++ src/DesktopStartUp.java
@@ -10,6 +10,7 @@ public class DesktopStartUp {
      * Initializes the game and launches the UI.
      */
     static public void main( String[] args ) {
+    	System.out.println("Start the  game...ok!");
         throw new UnsupportedOperationException();
     }
 }
