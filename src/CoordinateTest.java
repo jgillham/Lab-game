16738afc6216553d76619c13eef65a2a05749937
fofo13 <fofosav85@hotmail.it>
2012-11-05 00:14:33 +0100

    Signed-off-by: fofo13 <fofosav85@hotmail.it>

--- src/CoordinateTest.java
+++ src/CoordinateTest.java
@@ -8,7 +8,13 @@ public class CoordinateTest extends TestCase {
 
 	@Test
 	public void testCoordinate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		  try {
+		      new Coordinate(2,0);
+		    }
+		    catch ( Exception e ) {
+		      fail( "Should not throw an exception." );
+		    }
 	}
 
 	@Test
