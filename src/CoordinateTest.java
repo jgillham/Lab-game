e30ea864eaf38ca532d1a9ed3e1b2af6fa53fe86
fofo13 <fofosav85@hotmail.it>
2012-11-06 22:52:43 +0100

    Signed-off-by: fofo13 <fofosav85@hotmail.it>

--- src/CoordinateTest.java
+++ src/CoordinateTest.java
@@ -10,7 +10,30 @@ public class CoordinateTest extends TestCase {
 	public void testCoordinate() {
 		//fail("Not yet implemented");
 		  try {
+			  /*decision's  tree
+			   *
+			   *first  set of inputs tests all bases cases
+			   *  
+			   * */
 		      new Coordinate(2,0);
+		      new Coordinate(0,-1);
+		      new Coordinate(2,-1);
+		      new Coordinate(-2,3);
+		      new Coordinate(0,0);
+		      new Coordinate(-2,-1);
+		   /*
+		    * second  set of inputs tests all limit cases
+		    * 
+		    */
+		      new Coordinate(2,999999999);
+		      new Coordinate(999999999,-1);
+		      new Coordinate(999999999,999999999);
+		      new Coordinate(2,-999999999);
+		      new Coordinate(-999999999,-1);
+		      new Coordinate(-999999999,-999999999);
+		   
+
+
 		    }
 		    catch ( Exception e ) {
 		      fail( "Should not throw an exception." );
